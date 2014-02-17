a3a4984a28fabec0bce2fe3bfb6d29b2cbbb427e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-02-17 22:00:23 +0200

    Checkboxradio: Test input set calculation

---
+++ tests/unit/checkboxradio/input_set_tests_core.js
@@ -0,0 +1,36 @@
+( function( $, undefined ) {
+
+test( "Radio groups are correctly identified", function() {
+	var theDocument = $( document );
+		groups = {
+			"#radio\\:1": "#radio\\:1,#radio\\:8",
+			"#radio\\:2": "#radio\\:2,#radio\\:3,#radio\\:6",
+			"#radio\\:3": "#radio\\:2,#radio\\:3,#radio\\:6",
+			"#radio\\:6": "#radio\\:2,#radio\\:3,#radio\\:6",
+			"#radio\\:4": "#radio\\:4,#radio\\:7",
+			"#radio\\:7": "#radio\\:4,#radio\\:7",
+			"#radio\\:5": "#radio\\:5",
+			"#radio\\:8": "#radio\\:1,#radio\\:8"
+		};
+
+	$.each( groups, function( index, value ) {
+		var result,
+			radio = $( index ),
+			group = $( value );
+
+		result = $.mobile.checkboxradio.prototype._getInputSet.call({
+			element: radio,
+			inputtype: "radio",
+			document: theDocument
+		});
+		deepEqual( group.length, result.length,
+			index + ": length of group is correct" );
+		group.each( function() {
+			deepEqual( result.filter( this ).length, 1,
+				index + ": " + $( this ).attr( "id" ) +
+					" is correctly present in the result" );
+		});
+	});
+});
+
+})( jQuery );
